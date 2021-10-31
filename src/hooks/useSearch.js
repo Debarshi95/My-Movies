57c5c8e75cb8ec78f3153d4430ee2bee4ebf24b1
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-10-31 22:41:57 +0530

    Minor Fix

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -5,6 +5,7 @@ import { request, headers } from '../config/config';
 const useSearch = ({ query, type }) => {
   const [searchedData, setSearchedData] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState('');
   React.useEffect(() => {
     const abortController = new AbortController();
     if (query === '') return;
@@ -14,17 +15,19 @@ const useSearch = ({ query, type }) => {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setIsLoading(false);
         setSearchedData(data.results);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
     return function cleanup() {
       abortController.abort();
     };
   }, [query, type]);
 
-  return { isLoading, searchedData };
+  return { isLoading, searchedData, error };
 };
 
 export default useSearch;
