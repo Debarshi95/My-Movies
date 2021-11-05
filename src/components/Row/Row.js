40c664d79df03036e6d77cfde9c0223d21d5664c
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-11-05 21:42:12 +0530

    Created actions, selectors for Home. Update Home, Row to integrate dispatch and selectors

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
+import { isEqual } from 'lodash';
+import { useSelector } from 'react-redux';
 import PosterCard from '../PosterCard/PosterCard';
 import './Row.css';
 
-function Row({ results, title }) {
+function Row({ selector, title, action, type }) {
+  const data = useSelector(selector);
+
+  useEffect(() => {
+    const isSameType = isEqual(type, data?.itemType);
+    if (!isSameType) {
+      action(type);
+    }
+  }, [action, data?.itemType, type]);
+
   return (
-    <div className="row__container">
-      <h2>{title}</h2>
-      <div className="row">
-        {results.length > 0 ? (
-          results.map((result) => <PosterCard key={result.id} item={result} />)
-        ) : (
-          <p>We donot have enough data to show</p>
-        )}
+    <div className="row__root">
+      <h3 className="row__title">{title}</h3>
+      <div className="row__container">
+        {data?.data?.results?.map((item) => (
+          <PosterCard
+            key={item.id}
+            itemId={item.id}
+            posterPath={item.poster_path}
+            alt={item.title || item.name}
+            type={type}
+          />
+        ))}
       </div>
     </div>
   );
 }
 Row.propTypes = {
-  results: PropTypes.objectOf(Array).isRequired,
+  selector: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
 };
-export default Row;
+
+Row.whyDidYouRender = true;
+export default memo(Row);
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,37 +1,70 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { injectSaga } from 'redux-injectors';
+import { compose } from 'redux';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import homeSaga from '../../store/sagas/homeSaga';
 import Carousel from '../../components/Carousel/Carousel';
 import Genres from '../../components/Genres/Genres';
 import Row from '../../components/Row/Row';
-import { request } from '../../config/config';
+import { Creators } from '../../store/actions/commonActions';
+import {
+  selectPopular,
+  selectTopRated,
+  selectUpcomingMovies,
+  selectUpcomingShows,
+} from '../../store/selectors/homeSelector';
 import './Home.css';
-import { TypeContext } from '../../providers/TypeProvider';
-import withRow from '../../hoc/withRow';
 
 function Home() {
-  const { type } = React.useContext(TypeContext);
-  const RowComp = withRow(Row);
+  const { pathname } = useLocation();
+  const itemType = pathname.split('/')[1] || 'movie';
+
+  const dispatch = useDispatch();
+  const {
+    requestGetPopular,
+    requestGetTopRated,
+    requestGetUpcomingMovies,
+    requestGetUpcomingShows,
+  } = Creators;
+
+  console.log('Home rendered');
   return (
     <div className="home">
-      <Carousel />
-      <div className="home__wrapper">
-        <Genres />
-        <RowComp url={request.getPopular(type)} title="Popular" />
-        <RowComp url={request.getTopRated(type)} title="Most Rated" />
-        {type === 'movie' ? (
-          <>
-            <RowComp url={request.getNowPlayingMovies()} title="Now Playing" />
-            <RowComp url={request.getUpComingMovies()} title="Upcoming" />
-          </>
-        ) : null}
-
-        {type === 'tv' ? (
-          <>
-            <RowComp url={request.getAiringToday()} title="Airing Today" />
-            <RowComp url={request.getOnTheAir()} title="On The Air" />
-          </>
-        ) : null}
+      <Carousel type={itemType} />
+      <div className="home__container">
+        <Genres type={itemType} />
+        <Row
+          title="Popular"
+          type={itemType}
+          selector={selectPopular}
+          action={() => dispatch(requestGetPopular(itemType))}
+        />
+        <Row
+          title="Top Rated"
+          type={itemType}
+          selector={selectTopRated}
+          action={() => dispatch(requestGetTopRated(itemType))}
+        />
+        {itemType === 'movie' ? (
+          <Row
+            title="Upcoming"
+            type={itemType}
+            selector={selectUpcomingMovies}
+            action={() => dispatch(requestGetUpcomingMovies())}
+          />
+        ) : (
+          <Row
+            title="On The Air"
+            type={itemType}
+            selector={selectUpcomingShows}
+            action={() => dispatch(requestGetUpcomingShows())}
+          />
+        )}
       </div>
     </div>
   );
 }
-export default Home;
+
+Home.whyDidYouRender = true;
+export default compose(memo, injectSaga({ key: 'home', saga: homeSaga }))(Home);
